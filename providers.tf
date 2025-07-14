e70499ed07123dc20f0f391ccc2817eb42928c50
himanshu <rathihimanshu@live.com>
2025-07-14 20:55:08 +0530

    one

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-lock-demo"
+    storage_account_name = "tfstatelock5997"
+    container_name       = "tfstate"
+    key                  = "lockednew/demo.tfstate"
+  }
+}
+resource "azurerm_resource_group" "rgst1" {
+  name     = "githubaction1"
+  location = "East US"
+
+}
